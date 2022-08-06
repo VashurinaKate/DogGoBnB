bd78c0ffa138a2e6930f1c7c10f34c36e6e2b766
Дмитрий Гапоненко <damon3453@yandex.ru>
2022-08-07 01:06:22 +0300

    [add] real user

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -15,6 +15,14 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
+        User::create([
+            'name' => 'Kate',
+            'email' => 'kate@gmail.com',
+            'email_verified_at' => now(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'role' => 10,
+        ]);
+
         User::factory()->count(30)->create();
     }
 }
