0224bf92c41793983302a534e0ca0a4bac6e0e24
Aleksej-F <l7933@yandex.ru>
2022-09-14 23:19:01 +0300

    cors

---
+++ app/Http/Middleware/Cors.php
@@ -0,0 +1,26 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class Сors 
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  Request  $request
+     * @param  \Closure(Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\JsonResponse|\Illuminate\Http\RedirectResponse
+     */
+    public function handle($request, Closure $next) {
+   
+        return $next($request)
+          ->header('Access-Control-Allow-Origin', '*')
+          ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+          ->header('Access-Control-Allow-Headers',' Origin, Content-Type, Accept, Authorization, X-Request-With')
+          ->header('Access-Control-Allow-Credentials',' true');
+    }
+}
